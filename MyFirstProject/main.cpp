9cb5305b469e44817d36615fd3dca270869729b3
PedroP54 <112002353+PedroP54@users.noreply.github.com>
2022-08-29 08:18:42 -0700

    Add project files.

---
+++ MyFirstProject/main.cpp
@@ -0,0 +1,16 @@
+/*
+Pedro Aragon
+Class Fall 2022
+Due: Aug 31, 2022
+Lab 1 Introduction to C++ Programation
+This lab is a demo of my ability to work with an IDE and GitHub
+*/
+
+#include <iostream>
+
+int main()
+{
+	std::cout << "Hello World!" << " Orange";
+
+		return 0;
+}
